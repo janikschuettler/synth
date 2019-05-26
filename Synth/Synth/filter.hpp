125bdc5eb72668e42514617559cf4018d2ba7b99
Janik S <janiks@ethz.ch>
2019-05-26 14:31:25 +0200

    Initial commit

---
+++ Synth/Synth/filter.hpp
@@ -0,0 +1,14 @@
+//
+//  filter.hpp
+//  Synth
+//
+//  Created by Janik Schütter on 5/26/19.
+//  Copyright © 2019 Janik Schütter. All rights reserved.
+//
+
+#ifndef filter_hpp
+#define filter_hpp
+
+#include <stdio.h>
+
+#endif /* filter_hpp */
